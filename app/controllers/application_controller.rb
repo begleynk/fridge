dff1907e2597785f50e3f5876d498d03d2f03ac0
Niklas Begley <niklas.begley@gmail.com>
2014-01-20 13:04:15 +0000

    Fixed bredcrumb issues with new actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :resolve_layout
+
+
+  private
+
+  def resolve_layout
+    case action_name
+    when "new"
+      "new_form_layout"
+    else
+      "application"
+    end
+  end
 end
